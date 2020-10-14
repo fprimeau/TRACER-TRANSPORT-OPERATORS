7b99b9588b74845b7493f422f1a6567674049e7f
Weiwei Fu <weiweif@gplogin2.gp.local>
2020-10-14 16:41:45 -0700

    Weiwei Initial commit

---
+++ mkNC.m
@@ -0,0 +1,62 @@
+function []=mkNC(name,outfile,IFcells)
+
+% define MPAS resolution here
+% dimensions of 240 km test case:
+%         nEdges = 5754 ;
+%         nCells = 1794 ;
+%         TWO = 2 ;
+%         maxEdges = 10 ;
+%         nVertLevels = 40 ;
+%         StrLen = 64 ;
+%         Time = UNLIMITED ; // (1 currently)
+%         maxEdges2 = 20 ;
+%         nVertices = 3945 ;
+%         vertexDegree = 3 ;
+
+
+% setup netcdf format
+ncformat = '64bit';
+
+% the dimension info is read from file 'name'
+ f = ncinfo(name);
+ for i = 1:length(f.Dimensions)
+     nccreate(outfile,f.Dimensions(i).Name,'Dimensions',{f.Dimensions(i).Name,f.Dimensions(i).Length},'format',ncformat);
+ end
+
+[filepath,bname,ext] = fileparts(f.Filename);
+LL = structfind(f.Dimensions,'Name','nCells');
+nCells_len = f.Dimensions(LL).Length;
+LL = structfind(f.Dimensions,'Name','nVertLevels');
+nVertL_len = f.Dimensions(LL).Length;
+LL = structfind(f.Dimensions,'Name','Time');
+nTime_len = f.Dimensions(LL).Length;
+disp(['nCells: ' num2str(nCells_len) ' nVertLevels: ' num2str(nVertL_len) ' Time ' num2str(nTime_len)]);
+
+% define global attributes
+ ncwriteatt(outfile,'/','IRF initialization','tracers with 1s and 0s') ;
+ ncwriteatt(outfile,'/','Grid','MPAS-O unstructured');
+ ncwriteatt(outfile,'/','resolution',bname);
+%  for i = 1:length(f.Attributes)
+%      ncwriteatt(outfile,f.Attributes(i).Name,'/',f.Attributes(i).Value);
+%  end
+
+IRF = permute(IFcells,[2 1 3]);
+% define variables
+for i=1:size(IRF,3)
+% for i=1:2
+  scrsh = sprintf('%s %d %s %s','now writing IRF',i,'into file ',outfile);
+  disp(scrsh);
+  nccreate(outfile,['IRF_' num2str(i)],'Dimensions',{'nVertLevels',60,'nCells',235160,'Time',1},'format',ncformat);
+  ncwrite(outfile,['IRF_' num2str(i)],IRF(:,:,i),[1 1 1]);
+  ncwriteatt(outfile,['IRF_' num2str(i)],'long_name',['IRF tracers number ' num2str(i)]);
+end
+
+% define dimensions variables
+% nccreate(name,'nVertices','Dimensions',{'nVertices',inf},'format','classic');
+% nccreate(name,'vertexDegree','Dimensions',{'vertexDegree',inf},'format','classic');
+
+
+% define variables
+% nccreate(name,'lat','Dimensions',{'j',jlat,'i',ilon},'format','classic');
+% ncwrite(name,'lat',lat,[1 1]);
+% ncwriteatt(name,'lat','long_name','Latitude in POP grid');
---
+++ mkS.m
@@ -0,0 +1,42 @@
+% set testcase resolution (KM) for world ocean, available
+% are: 240km, 120km, 60km, 30km, 15km
+  resolution   = '120kmN';
+
+% read restart file from test cases
+  test_dir     = '/DFS-L/SCRATCH/moore/weiweif/MPAS-O/TestCases/';
+  test_case    = ['MPAS-O_V6.0_EC60to30/'];
+  restart_file = 'oEC60to30v3_60layer.170905.nc';
+  filename     = [test_dir test_case restart_file];
+
+% read grid information: lonCell, latCell, nEdgesOnCell, cellsOnCell
+
+  % mpas = ncReadAll(filename,'get',[24 19 9 5 29]);
+  % [...] the indices of needed variables
+  mpas.lonCell        = ncread(filename,'lonCell',[1],[inf]);
+  mpas.latCell        = ncread(filename,'latCell',[1],[inf]);
+  mpas.refBottomDepth = ncread(filename,'refBottomDepth',[1],[inf]);
+  mpas.nEdgesOnCell   = ncread(filename,'nEdgesOnCell',[1],[inf]);
+  mpas.cellsOnCell    = ncread(filename,'cellsOnCell',[1 1],[inf inf]);
+
+% find how many trancers are needed for Tracer-Transport Matrix
+  c = double(mpas.cellsOnCell); % imax = max(c(:));
+  maxEdges = size(mpas.cellsOnCell,1);
+  imax = max(c(:));
+  n = mpas.nEdgesOnCell;
+  k = 1;
+  for i = 1:imax; % loop over cell indices
+    % cc = setdiff(c(1:n(i),i),[0]);
+    % a cell is its own neighbor (index is added)
+    cc = setdiff([i; c(1:n(i),i)],[0]);
+    for j = 1:length(cc)
+      ix(k) = i;
+      iy(k) = cc(j);
+      s(k) = 1;
+      k = k+1;
+    end
+  end
+  S = sparse(ix,iy,s);
+  tic
+  gout = mycolgroup(S);
+  toc
+  disp([num2str(max(gout(:))) ' IRF tracers are required to build TTM']);
